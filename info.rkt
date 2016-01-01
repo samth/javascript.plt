51e70a982b1a8b38bacda493498fb7601e9b1fc3
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2016-01-01 16:36:44 -0500

    Deps.

---
+++ info.rkt
@@ -0,0 +1,26 @@
+#lang info
+(define collection "javascript")
+(define name "JavaScript")
+(define blurb
+  (list "An implementation of JavaScript as a Racket language."))
+(define scribblings '(("scribblings/javascript.scrbl" (multi-page))))
+(define categories '(devtools))
+(define version "0.18")
+(define deps '("base"
+               "compatibility-lib"
+               "drracket-plugin-lib"
+               "gui-lib"
+               "parameter"
+               "parser-tools-lib"
+               "planet-lib"
+               "scheme-lib"
+               "set"
+               "srfi-lite-lib"
+               "string-constants-lib"
+               "unstable-contract-lib"))
+(define build-deps '("in-new-directory"
+                     "parser-tools-doc"
+                     "racket-doc"
+                     "rackunit-lib"
+                     "scribble-lib"
+                     "unstable-doc"))
--- info.ss
+++
@@ -1,8 +0,0 @@
-#lang info
-(define collection "javascript")
-(define name "JavaScript")
-(define blurb
-  (list "An implementation of JavaScript as a Racket language."))
-(define scribblings '(("scribblings/javascript.scrbl" (multi-page))))
-(define categories '(devtools))
-(define version "0.18")
